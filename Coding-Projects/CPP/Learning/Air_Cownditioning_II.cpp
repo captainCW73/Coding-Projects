fd2db491ea0483669fe757645d65ddf56c830e12
captainCW73 <chasewong2012@gmail.com>
2026-04-21 20:28:56 -0400

    update

--- Coding-Projects/CPP/Learning/Air_Cownditioning_II.cpp
+++ Coding-Projects/CPP/Learning/Air_Cownditioning_II.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
+
 int main() {
   int n, m_limit;
   cin >> n >> m_limit;
+
   for (int i = 0; i < n; i++) {
-    int x;
-    cin >> x;
+    int s, t, c;
+    cin >> s >> t >> c;
+    cout << t << "\n";
   }
+
+  for (int i = 0; i < m_limit; i++) {
+    int a, b, p, m;
+    cin >> a >> b >> p >> m;
+  }
+
   return 0;
 }
